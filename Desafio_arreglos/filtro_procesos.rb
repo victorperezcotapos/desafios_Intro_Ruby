2e4bf92be7ba3257a4830fc4c3a0862773f4dcdb
Victor <vperezco@uc.cl>
2019-04-15 19:57:57 -0400

    filtro_procesos.rb first version

---
+++ Desafio_arreglos/filtro_procesos.rb
@@ -0,0 +1,4 @@
+data = open('procesos.txt').readlines
+data_int=data.map {|e| e.to_i}
+procesos_filtrados = data_int.select { |e| e>ARGV[0].to_i}
+File.write('procesos_filtrados.data',procesos_filtrados)
